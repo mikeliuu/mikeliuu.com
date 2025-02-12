0548b8ab56307bcc187ad969d45ab3c93939601d
Mike Liu <47265796+mikeliuu@users.noreply.github.com>
2025-02-12 12:43:43 +0000

    fix: eslint with &apos;

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,13 @@ export default function About() {
       </h1>
 
       <p className="leading-loose">
-        Hi, I'm Mike, an experienced web developer specializing in frontend technologies such as ReactJS, JavaScript, and TypeScript. I'm passionate about crafting user-friendly interfaces and have a proven track record of delivering high-quality solutions that enhance the user experience. In my free time, I enjoy playing football, exploring new destinations, and recently discovered a passion for snowboarding. Feel free to reach out if you share any of these interests!
+        Hi, I&apos;m Mike, an experienced web developer specializing in frontend
+        technologies such as ReactJS, JavaScript, and TypeScript. I&apos;m
+        passionate about crafting user-friendly interfaces and have a proven
+        track record of delivering high-quality solutions that enhance the user
+        experience. In my free time, I enjoy playing football, exploring new
+        destinations, and recently discovered a passion for snowboarding. Feel
+        free to reach out if you share any of these interests!
       </p>
     </Card>
   );
